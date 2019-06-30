86fae16a1e9266af97dd5eb423b37758ee3e0fbf
Muhammad Ateeb Aslam <30898533+ateeb327@users.noreply.github.com>
2019-07-01 01:25:30 +0500

    Distributed Iot Example using MQTT
    This example utilizes MQTT Publish and Subscribe Mechanisms to demonstrate how MQTT works in a distributed IoT environment.

---
+++ Lec-10 Distributed IoT/CLoudMQTT_Subsc.ino
@@ -0,0 +1,113 @@
+/*
+Code Provided by UrduITAcademy
+
+Install necessary Libraries First
+This example demonstrates MQTT protocol. It takes values from 
+DHT11 Temperature and humidity sensor and publish those to a MQTT broker. 
+A subscriber MCU rceives the values and turn on an LED if values
+exceed from a certain threshhold. 
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>;
+
+
+//Variables
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+// Update these with values suitable for your network.
+
+const char* ssid = "Learner";
+const char* password = "wilco@07";
+const char* mqtt_server = "m14.cloudmqtt.com";
+
+void setup() {
+  pinMode(12, OUTPUT);  // Initialize the 12 pin as an output-NodeMCu Pin D6
+  Serial.begin(115200);
+  dht.begin();             //initialize DHT sensor
+  setup_wifi();
+  client.setServer(mqtt_server, 16395);
+  client.setCallback(callback);
+}
+
+//WiFi Setup Function
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+//Wifi Setup Function ends here
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(12, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else if ((char)payload[0] == '0') {
+    digitalWrite(12, LOW);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266SubscriberClient","gqbybsmm","ziEEPqVW0rDH")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.subscribe("UITAtopic");
+      
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+ 
+
+}
+
+
---
+++ Lec-10 Distributed IoT/CLoudMQTT_publish.ino
@@ -0,0 +1,115 @@
+/*
+Code Provided by UrduITAcademy
+
+Install necessary Libraries First
+This example demonstrates MQTT protocol. It takes values from 
+DHT11 Temperature and humidity sensor and publish those to a MQTT broker. 
+A subscriber MCU rceives the values and turn on an LED if values
+exceed from a certain threshhold. 
+*/
+//This code publishes 1 to the topic "UITAtopic" if the temp or
+// humidity exceeds a specific value 
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>;
+
+//constants
+#define DHTPIN 2     // what pin we're connected to -Wemos Pin D4
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor 
+
+//Variables
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+WiFiClient espClient;
+PubSubClient client(espClient);
+// Update these with values suitable for your network.
+
+const char* ssid = "Learner";
+const char* password = "wilco@07";
+const char* mqtt_server = "broker.hivemq.com";
+
+void setup() {  // Initialize the 12 pin as an output-NodeMCu Pin D6
+  Serial.begin(115200);
+  dht.begin();             //initialize DHT sensor
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+}
+
+//WiFi Setup Function
+void setup_wifi() {
+  WiFi.mode(WIFI_STA);
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+//Wifi Setup Function ends here
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) { //cloud mqtt: gqbybsmm,ziEEPqVW0rDH
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      if(temp >= 15 || hum >= 100){
+      client.publish("UITAtopic", "1");//publish 1 if temp or humidity increases
+      }
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+//function to read temperature and humidity from DHT sensor
+void dht_read(){
+  hum = dht.readHumidity();
+    temp= dht.readTemperature();
+  }
+//DHT function ends here
+
+
+void loop() {
+  dht_read();
+  print_dht();
+  
+  if (!client.connected()) {
+    reconnect();
+  }
+  //client.loop();
+  
+    Serial.print("Published message");
+    //Serial.println(msg);
+    if(temp >= 15 || hum >= 100){
+    client.publish("UITAtopic", "1");  //publish message on specific topic
+  }
+}
+
+//Function to print DHT values to Serial monitor
+void print_dht(){
+  Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+    delay(1000);
+  }
